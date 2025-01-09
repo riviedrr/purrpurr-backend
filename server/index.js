38c3498d4382ebd5a7a9efb2927a0373574a0354
riviedrr <helen761243762@yandex.ru>
2025-01-09 22:58:36 +0500

    auth+middlewares commit

--- server/index.js
+++ server/index.js
@@ -3,9 +3,9 @@ const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
-const fileUpload = require('express=fileUpload')
+const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
-const errorHandler = require('./middleware/ErrorHandingMiddleware')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 const path = require('path')
 
 const PORT = process.env.PORT || 5000
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.static())
+app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
 app.get('/', (req, res) => {
     res.status(200).json({message: 'WORKING!!!'})
